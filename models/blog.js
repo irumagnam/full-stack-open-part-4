fb6f48be07972a3f974f97812bae3e3e7d77c12b
Seetha Mangamuri <manga.muri@hotmail.com>
2023-09-21 09:12:06 -0700

    applying required:true for mandatory columns

--- models/blog.js
+++ models/blog.js
@@ -2,10 +2,21 @@ const mongoose = require('mongoose')
 
 // declare schema
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
